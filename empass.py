6fbba1f992106594a22187645e2d04f68bf20ce7
Scott Chacon <github.user@gmail.com>
2008-01-06 09:25:52 +0000

    Removed the old "alpha" dictionary.  Now using a unified "alphadict" variable for the entire program.

--- empass.py
+++ empass.py
@@ -55,14 +55,14 @@ righthand = 'plokmjnuhbyPLOKMJNUHBY67890^&*()_+-=[]\;:<>/?,.'
 #All keys avalible from the left hand
 lefthand = 'qazwsxedcrfvtgbQAZWSXEDCRFVTGB2345!@#$%'
 #The alphabet
-alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 alphadict = {'a':'1','b':'2','c':'3','d':'4','e':'5','f':'6','g':'7','h':'8','i':'9','j':'10','k':'11','l':'12','m':'13','n':'14','o':'15','p':'16','q':'17','r':'18','s':'19','t':'20','u':'21','v':'22','w':'23','x':'24','y':'25','z':'26'}
 
 def usage():
     print __doc__
 
 def alphagen():
- alphastr = string.join(alpha)
+# Dicts are not sorted, it comes out like a c b e d ... so sort your dicts
+ alphastr = string.join(sorted(alphadict.keys()))
  return alphastr
 
 def rowgen(phrase): 
